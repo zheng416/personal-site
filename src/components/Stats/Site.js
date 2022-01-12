47b9ffbe855f42e9f43bc22ac93b23ffbf2b4eae
Jacky Zheng <zheng416@purdue.edu>
2022-01-11 20:11:45 -0500

    Deploy attempt

--- src/components/Stats/Site.js
+++ src/components/Stats/Site.js
@@ -9,7 +9,7 @@ const Stats = () => {
   const fetchData = useCallback(async () => {
     // request must be authenticated if private
     const res = await fetch(
-      'https://api.github.com/repos/mldangelo/personal-site',
+      'https://api.github.com/repos/zheng416/personal-site',
     );
     const resData = await res.json();
     setResponseData(
